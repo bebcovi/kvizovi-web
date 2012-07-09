042800aaec789c0430a916a25e0bfa4ac6ee4f03
Matija Marohnić <matija.marohnic@gmail.com>
2012-07-09 20:45:29 +0200

    Set up assets

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,14 @@
+//= require zepto.min
+//
+//= require underscore
+//= require backbone
+//
+//= require hamlcoffee
+//
+//= require .//lektire
+//
+//= require_tree ../templates/
+//= require_tree .//models
+//= require_tree .//collections
+//= require_tree .//views
+//= require_tree .//routers
---
+++ app/assets/javascripts/lektire.js.coffee
@@ -0,0 +1,9 @@
+window.Lektire =
+  Models: {}
+  Collections: {}
+  Views: {}
+  Routers: {}
+  init: -> console.log 'Hello from Backbone!'
+
+$(document).ready ->
+  Lektire.init()
