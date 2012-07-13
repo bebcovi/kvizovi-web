1b877be78e0f682fcf6702b378ede20040482196
Janko Marohnić <janko.marohnic@gmail.com>
2012-07-13 12:31:45 +0200

    Finish question form

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,35 @@
 class Question < ActiveRecord::Base
   attr_accessible :content, :correct_answer, :points, :type
-  attr_accessor :answer
+
+  # Virtual attributes
+  attr_accessor :boolean, :choice, :association, :photo, :text
+  attr_accessible :boolean, :choice, :association, :photo, :text
+  serialize :correct_answer
   serialize :answers
 
   belongs_to :quiz
   belongs_to :book
+
+  before_create :write_answers
+
+  def kind
+    TYPES[read_attribute(:kind)]
+  end
+
+  private
+
+  def write_answers
+    case kind
+    when :boolean
+      self.correct_answer = boolean
+    when :choice
+      self.correct_answer = choice.first
+      self.answers = choice
+    when :association
+      self.correct_answer = Hash[association[:left_column].zip(association[:right_column])]
+      self.answers = [association[:left_column], association[:right_column]]
+    end
+  end
 
   TYPES = {
     1 => :boolean,
