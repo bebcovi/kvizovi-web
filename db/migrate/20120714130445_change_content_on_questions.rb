d74fd612a789c53445f7f9c750517607d4ea8ea6
Janko Marohnić <janko.marohnic@gmail.com>
2012-07-14 15:07:43 +0200

    Change Question#content to text in the database

---
+++ db/migrate/20120714130445_change_content_on_questions.rb
@@ -0,0 +1,9 @@
+class ChangeContentOnQuestions < ActiveRecord::Migration
+  def up
+    change_column :questions, :content, :text
+  end
+
+  def down
+    change_column :questions, :content, :string
+  end
+end
