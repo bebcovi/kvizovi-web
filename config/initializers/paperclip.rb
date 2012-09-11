87aca76a688f9e68a6c2ee2607f9f03dcdecf47d
Janko Marohnić <janko.marohnic@gmail.com>
2012-09-11 23:21:55 +0200

    Fix Dropbox uploading

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,13 +1,16 @@
-ActiveRecord::Base.extend Module.new do
-  def has_attached_file(name, options = {})
-    if Rails.env.development?
-      super
-    else
-      dropbox_options = {
-        storage: :dropbox,
-        dropbox_credentials: "#{Rails.root}/config/dropbox.yml"
-      }
-      super(name, dropbox_options.merge(options))
+class ActiveRecord::Base
+  class << self
+    alias normal_has_attached_file has_attached_file
+    def has_attached_file(name, options = {})
+      if Rails.env.development?
+        normal_has_attached_file(name, options)
+      else
+        dropbox_options = {
+          storage: :dropbox,
+          dropbox_credentials: "#{Rails.root}/config/dropbox.yml"
+        }
+        normal_has_attached_file(name, dropbox_options.merge(options))
+      end
     end
   end
 end
