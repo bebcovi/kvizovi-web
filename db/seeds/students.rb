0627dd422519b5aff0f1bac325034122521ba87c
Matija Marohnić <matija.marohnic@gmail.com>
2012-09-18 20:40:47 +0200

    Edit seed

--- db/seeds/students.rb
+++ db/seeds/students.rb
@@ -3,18 +3,10 @@
 school_key = School.find_by_username("mioc").key
 Student.create! [
   {
-    first_name: "Janko",
-    last_name: "Marohnić",
-    username: "janko",
-    password: "janko",
-    grade: "4",
-    school_key: school_key
-  },
-  {
-    first_name: "Matija",
-    last_name: "Marohnić",
-    username: "matija",
-    password: "matija",
+    first_name: "Pero",
+    last_name: "Perić",
+    username: "pero",
+    password: "pero",
     grade: "3",
     school_key: school_key
   }
