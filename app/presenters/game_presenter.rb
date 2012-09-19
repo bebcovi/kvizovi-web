4cbfe7c26d35101dc7dedbcb8ada98b8a597c64e
Janko Marohnić <janko.marohnic@gmail.com>
2012-09-19 21:38:30 +0200

    Add ranks

--- app/presenters/game_presenter.rb
+++ app/presenters/game_presenter.rb
@@ -2,10 +2,22 @@ class GamePresenter < BasePresenter
   presents :game
 
   def results
-    game.players.zip(game.scores, 1..game.players.count)
+    game.players.zip(game.scores, 1..game.players.count, game.scores.map { |score| rank(score) })
   end
 
   def total_score
     game.questions.count / game.players.count
+  end
+
+  def rank(score)
+    percent = percentage(score, game.questions.count)
+    case percent
+    when 0...30
+      "znalac-malac"
+    when 30...70
+      "ekspert"
+    when 70..100
+      "super-ekspert"
+    end
   end
 end
