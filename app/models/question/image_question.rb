52383d3cce478fb0efe16347a5542ac5df61877a
Janko Marohnić <janko.marohnic@gmail.com>
2012-10-13 12:26:26 +0200

    Enable uploading images through URLs

--- app/models/question/image_question.rb
+++ app/models/question/image_question.rb
@@ -1,15 +1,33 @@
 require_relative "text_question"
 require "paperclip"
+require "uri"
 
 class ImageQuestion < TextQuestion
-  attr_accessible :image
+  attr_accessible :image_url, :image_file
+  attr_accessor :image_url, :image_file
 
   include Paperclip::Glue
   has_attached_file :image, styles: {resized: "x250>"}
 
+  before_validation :assign_image
+
+  validates_format_of :image_url, with: URI.regexp, allow_blank: true
   validates :image, attachment_presence: true
 
   def points
     3
+  end
+
+  private
+
+  def assign_image
+    if image_file.present?
+      self.image = image_file
+    elsif image_url.present?
+      begin
+        self.image = URI.parse(image_url)
+      rescue
+      end
+    end
   end
 end
--- spec/models/question/image_question_spec.rb
+++ spec/models/question/image_question_spec.rb
@@ -1,12 +1,6 @@
 require "spec_helper_lite"
 require_relative "../../../app/models/question/image_question"
 
-# Paperclip want to use this when setting the attachment to nil
-module Rails
-  def self.root
-  end
-end unless defined?(Rails)
-
 describe ImageQuestion do
   before(:each) { @it = build(:image_question) }
   subject { @it }
@@ -20,12 +14,38 @@ describe ImageQuestion do
   describe "#image" do
     its(:image) { should be_file }
     its(:image) { should respond_to(:url) }
+
+    it "can receive a URL" do
+      @it.image_url = "https://dl.dropbox.com/u/16783504/image.jpg"
+      @it.send(:assign_image)
+      @it.image_file_name.should eq "image.jpg"
+    end
   end
 
   describe "validations" do
+    before(:each) do
+      # Paperclip want to use this when setting the attachment to nil
+      stub_const("Rails", Module.new)
+      Rails.stub(:root)
+    end
+
     it "can't have a blank image" do
       @it.image = nil
       @it.should_not be_valid
+    end
+
+    it "validates the URL" do
+      @it.image_url = "invalid URL"
+      @it.should_not be_valid
+    end
+
+    it "accepts either file or an URL as the image" do
+      @it.image_url = "https://dl.dropbox.com/u/16783504/image.jpg"
+      @it.should be_valid
+
+      @it.image_url = nil
+      @it.image_file = Rack::Test::UploadedFile.new("#{ROOT}/spec/fixtures/files/image.jpg")
+      @it.should be_valid
     end
   end
 end
