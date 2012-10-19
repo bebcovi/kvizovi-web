861a0ebbb9c3ca1e0f76f0b8addb296b39b28b23
Janko Marohnić <janko.marohnic@gmail.com>
2012-10-19 21:40:19 +0200

    Fix some validation errors

--- app/models/question/data/choice_question_data.rb
+++ app/models/question/data/choice_question_data.rb
@@ -50,7 +50,7 @@ class ChoiceQuestionData < ActiveRecord::Base
 
   def validate_provided_answers
     if provided_answers.first.blank?
-      errors[:provided_answers] << "Prvi ponuđeni odgovor (koji će ujedno biti i točan odgovor) ne smije biti prazan."
+      errors[:provided_answers] << "Prvi ponuđeni odgovor ne smije biti prazan."
     end
   end
 end
