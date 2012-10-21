4d7d0e270e5bc406c4c954f362811af5ca6cdba5
Janko Marohnić <janko.marohnic@gmail.com>
2012-10-21 16:44:41 +0200

    Put all secret things into application.yml

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Lektire::Application.config.secret_token = 'b89e2362f591f445037ed45e31d508e07991ee9a8ef5b0ecdf83d452218ef30b7db6bd2419d209a35335f4e8f04325bb44924d31b7d1aa6ce49033d463218a4d'
+Lektire::Application.config.secret_token = ENV["SECRET_TOKEN"]
