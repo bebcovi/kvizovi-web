143ad2e5835f404421d7c0b180e43c93296cef8d
Janko Marohnić <janko.marohnic@gmail.com>
2012-10-21 19:56:04 +0200

    In grade picker reverse the order of schools

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -3,8 +3,8 @@
 module StudentsHelper
   def classes
     {
-      "Srednja škola" => ordinalize(1..4).zip(1..4),
-      "Osnovna škola" => ordinalize(1..8).zip(1..8)
+      "Osnovna škola" => ordinalize(1..8).zip(1..8),
+      "Srednja škola" => ordinalize(1..4).zip(1..4)
     }
   end
 end
