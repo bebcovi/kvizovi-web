f65d490b98ab267507fb875a3fd641c69e3ee567
Janko Marohnić <janko.marohnic@gmail.com>
2012-11-19 00:05:45 +0100

    Don't diplay anything from the admin school in /admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,10 +1,16 @@
 class AdminController < ApplicationController
   def index
-    @schools = School.scoped
-    @students = Student.scoped.includes(:school)
+    @schools = School.where("name <> '#{admin_school.name}'")
+    @students = Student.where("school_id <> #{admin_school.id}").includes(:school)
   end
 
   def school
     @school = School.find(params[:id])
+  end
+
+  private
+
+  def admin_school
+    @admin_school ||= School.find_by_name("Admin")
   end
 end
