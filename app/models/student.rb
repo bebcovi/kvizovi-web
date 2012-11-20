adf977e150b62914f0c1cc3ae655940fca1be9ad
Janko Marohnić <janko.marohnic@gmail.com>
2012-11-20 17:58:05 +0100

    Don't allow a grade without letter

--- app/models/student.rb
+++ app/models/student.rb
@@ -10,7 +10,7 @@ class Student < ActiveRecord::Base
 
   validate :validate_school_key, unless: :school_id?
   validates_format_of :username, with: /^[a-zA-Z0-9_]*$/
-  validates_format_of :grade, with: /^[0-8][a-z]?$/, allow_blank: true
+  validates_format_of :grade, with: /^[0-8][a-z]$/, allow_blank: true
   validates_length_of :username, minimum: 3
   validates_presence_of :first_name, :last_name, :username, :password, :gender, :grade, :year_of_birth
   validates_uniqueness_of :username
