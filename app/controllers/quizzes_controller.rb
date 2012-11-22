410108e6411f6c7184f1be846f71dd8371dc77cd
Matija Marohnić <matija.marohnic@gmail.com>
2012-11-23 00:29:33 +0100

    Minor stuff

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -37,9 +37,9 @@ class QuizzesController < ApplicationController
     quiz = current_user.quizzes.find(params[:id])
     quiz.toggle!(:activated)
     if quiz.activated?
-      flash[:notice] = "Kviz \"#{quiz}\" je aktiviran." + (quiz.grades.none? ? " Ali trenutno nije dostupan niti jednom razredu." : "")
+      flash[:notice] = %(Kviz "#{quiz}" je aktiviran") + (quiz.grades.none? ? ", ali trenutno nije dostupan niti jednom razredu." : ".")
     else
-      flash[:notice] = "Kviz \"#{quiz}\" je deaktiviran."
+      flash[:notice] = %(Kviz "#{quiz}" je deaktiviran.)
     end
     redirect_to quizzes_path
   end
