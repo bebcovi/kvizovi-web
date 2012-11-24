bacefdf1196e8b7a69df6e3a2e411fb5df2bf480
Janko Marohnić <janko.marohnic@gmail.com>
2012-11-24 13:19:29 +0100

    Fix a migration

--- db/migrate/20121124120426_transliterate_filenames.rb
+++ db/migrate/20121124120426_transliterate_filenames.rb
@@ -11,7 +11,7 @@ class TransliterateFilenames < ActiveRecord::Migration
   def up
     ActiveRecord::Base.record_timestamps = false
     ImageQuestion.all.each do |question|
-      question.update_attributes(:image_file_name, transliterate(question.image_file_name))
+      question.update_attributes(image_file_name: transliterate(question.image_file_name))
     end
     ActiveRecord::Base.record_timestamps = true
   end
