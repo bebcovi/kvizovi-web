85895ce9dd92fcfba26fe4ba37af60f073fe320a
Janko Marohnić <janko.marohnic@gmail.com>
2012-12-05 18:57:13 +0100

    Strip answer before comparing on text and image questions

--- app/models/question/text_question.rb
+++ app/models/question/text_question.rb
@@ -6,6 +6,6 @@ class TextQuestion < Question
   validates_presence_of :answer
 
   def correct_answer?(value)
-    answer.casecmp(value.chomp(".")) == 0
+    answer.chomp(".").strip.casecmp(value.chomp(".").strip) == 0
   end
 end
--- spec/models/question/text_question_spec.rb
+++ spec/models/question/text_question_spec.rb
@@ -10,16 +10,22 @@ describe TextQuestion do
   it { should be_a(Question) }
 
   describe "#has_answer?" do
+    before(:each) { @it.answer = "Answer" }
+
     it "is case insensitive" do
-      @it.correct_answer?(@it.answer.downcase).should be_true
+      @it.correct_answer?("answer").should be_true
     end
 
     it "ignores a potential period at the end" do
-      @it.correct_answer?(@it.answer + ".").should be_true
+      @it.correct_answer?("Answer.").should be_true
     end
 
     it "is false on incorrect answer" do
       @it.correct_answer?("Not the right answer").should be_false
+    end
+
+    it "ignores whitespace at the beginning and the end of answers" do
+      @it.correct_answer?(" Answer  ").should be_true
     end
   end
 
