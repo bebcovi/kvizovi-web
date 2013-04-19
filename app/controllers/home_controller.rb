af419d1785d6a31962602a08b44e8f3182d54691
Janko Marohnić <janko.marohnic@gmail.com>
2013-04-19 14:58:53 +0200

    Redirect from home if logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
+  before_filter :redirect_if_logged_in
+
   def index
+  end
+
+  private
+
+  def redirect_if_logged_in
+    if user_logged_in?
+      redirect_to root_url(sudomain: current_user.type)
+    end
   end
 end
