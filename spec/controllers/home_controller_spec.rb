a2d121ae9d8163243eedf7f34606d7d497a750aa
Janko Marohnić <janko.marohnic@gmail.com>
2013-04-20 20:28:17 +0200

    Make tests for redirection of home path

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,8 +2,18 @@ require "spec_helper"
 
 describe HomeController do
   describe "#index" do
-    it "doesn't raise errors" do
-      get :index
+    context "when user is not logged in" do
+      it "renders the template" do
+        get :index
+      end
+    end
+
+    context "when user is logged in" do
+      it "redirects if user is logged in" do
+        controller.send(:log_in!, Factory.create_without_validation(:empty_school))
+        get :index
+        expect(response).to be_a_redirect
+      end
     end
   end
 end
