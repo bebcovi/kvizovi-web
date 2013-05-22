0b201734564547664cffaa829e49b8f0667bebe9
Janko Marohnić <janko.marohnic@gmail.com>
2013-05-22 13:30:54 +0200

    Don't check if user is logged in when he tries to log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  before_filter only: [:new, :create] do
+  before_filter only: [:new] do
     redirect_to root_path_for(current_user) if user_logged_in?
   end
 
