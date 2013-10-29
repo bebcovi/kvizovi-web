326f32eae84ad0c465a244982c4e8f65724c6d20
Janko Marohnić <janko.marohnic@gmail.com>
2013-10-29 17:17:06 +0100

    Use a CDN in production
    Fixes #127

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Lektire::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "//d3dtvyxgtii4or.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
