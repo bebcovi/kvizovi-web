ca4dbeff9d05ccbc49e416af3fd1477f5e3dd6cc
Janko Marohnić <janko.marohnic@gmail.com>
2013-11-21 10:44:00 +0100

    Don't use CDN

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Lektire::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "//d9bybmcyeuhn7.cloudfront.net"
+  # config.action_controller.asset_host = "//d9bybmcyeuhn7.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
