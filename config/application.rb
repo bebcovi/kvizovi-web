01e31f6dd3cff95331c4b62bb1940107f0f5b3a1
Janko Marohnić <janko.marohnic@gmail.com>
2013-11-21 10:53:23 +0100

    Invalidate assets

--- config/application.rb
+++ config/application.rb
@@ -76,7 +76,7 @@ module Lektire
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
 
     config.assets.initialize_on_precompile = false
   end
