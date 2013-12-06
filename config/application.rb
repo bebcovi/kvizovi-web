f0811c30cdf677aef3b13e71c966e2509d8b8f41
Janko Marohnić <janko.marohnic@gmail.com>
2013-12-06 15:59:43 +0100

    Remove I18n's deprecation warning

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,8 @@ module Lektire
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :hr
 
+    I18n.enforce_available_locales = true
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
