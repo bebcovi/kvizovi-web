acdd0a7a81e390b22f14de24d05fb9d348916fee
Matija Marohnić <matija.marohnic@gmail.com>
2014-03-24 10:24:34 +0100

    Fix the image MIME type error

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,15 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  include ActionView::Helpers::AssetUrlHelper
 
   def default_url
-    case model
-    when Quiz
-      case version_name
-      when :small  then asset_path("assets/patterns/skulls/skulls-small.png")
-      when :medium then asset_path("assets/patterns/skulls/skulls-medium.png")
-      end
-    end
+   "patterns/skulls/skulls-#{version_name}.png" if model.is_a?(Quiz)
   end
 
   resize_to_fill 800, 800
