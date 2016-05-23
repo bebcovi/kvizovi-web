707637f003cd2ac7dad6eeef2538438ca0573c06
Matija Marohnić <matija.marohnic@gmail.com>
2016-05-23 11:16:42 +0200

    Extract callApi as a helper

---
+++ src/helpers/call-api.js
@@ -0,0 +1,28 @@
+import { normalize } from 'normalizr';
+import { camelizeKeys } from 'humps';
+import 'isomorphic-fetch';
+
+// Fetches an API response and normalizes the result JSON according to schema.
+// This makes every API response have the same shape, regardless of how nested it was.
+export default function callApi(endpoint, schema, method = 'get') {
+  return fetch(`${__API_URL__}/${endpoint}`, {
+    method,
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
+    .then(response => response.json().then(json => ({ json, response })))
+    .then(({ json, response }) => {
+      const camelizedJson = camelizeKeys(json);
+
+      if (!response.ok) {
+        return Promise.reject(camelizedJson.errors);
+      }
+
+      return normalize(camelizedJson, schema);
+    })
+    .then(
+      response => ({ response }),
+      errors => ({ errors })
+    );
+}
--- src/services/api.js
+++ src/services/api.js
@@ -1,31 +1,5 @@
-import { Schema, arrayOf, normalize } from 'normalizr';
-import { camelizeKeys } from 'humps';
-import 'isomorphic-fetch';
-
-// Fetches an API response and normalizes the result JSON according to schema.
-// This makes every API response have the same shape, regardless of how nested it was.
-function callApi(endpoint, schema, method = 'get') {
-  return fetch(`${__API_URL__}/${endpoint}`, {
-    method,
-    headers: {
-      'content-type': 'application/json',
-    },
-  })
-    .then(response => response.json().then(json => ({ json, response })))
-    .then(({ json, response }) => {
-      const camelizedJson = camelizeKeys(json);
-
-      if (!response.ok) {
-        return Promise.reject(camelizedJson.errors);
-      }
-
-      return normalize(camelizedJson, schema);
-    })
-    .then(
-      response => ({ response }),
-      errors => ({ errors })
-    );
-}
+import { Schema, arrayOf } from 'normalizr';
+import callApi from '../helpers/call-api';
 
 // We use this Normalizr schemas to transform API responses from a nested form
 // to a flat form where repos and users are placed in `entities`, and nested
